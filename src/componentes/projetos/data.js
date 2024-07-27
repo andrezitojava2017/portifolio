ea1b0f88b1ec55eee1895c3cf495178c8222d409
Jederson Andre <andre_sjx@live.com>
2024-07-26 23:31:19 -0300

    :sparkles: feat: implementado logica e inclusao logos das tecnologias de cada projeto

--- src/componentes/projetos/data.js
+++ src/componentes/projetos/data.js
@@ -1,3 +1,13 @@
+import { FaHtml5, FaGitAlt } from 'react-icons/fa';
+import { RiNextjsFill } from 'react-icons/ri';
+import { BiLogoTypescript } from 'react-icons/bi';
+import { RiJavascriptFill } from 'react-icons/ri';
+import { RiSupabaseFill } from 'react-icons/ri';
+import { FaReact } from 'react-icons/fa';
+import { SiMongodb, SiFirebase } from 'react-icons/si';
+import { IoLogoNodejs } from 'react-icons/io5';
+import { SiExpress } from 'react-icons/si';
+
 export const listaProjetos = [
   {
     id: 1,
@@ -5,6 +15,38 @@ export const listaProjetos = [
     descricao:
       'Sistema Web para controle de prestadores e geração de guias de INSS para terceiros',
     href: 'https://github.com/andrezitojava2017/prestador-web.git',
+    tech: [
+      {
+        icon: FaHtml5,
+        size: 50,
+        color: '#E65100',
+      },
+      {
+        icon: RiNextjsFill,
+        size: 50,
+        color: 'cyan',
+      },
+      {
+        icon: BiLogoTypescript,
+        size: 50,
+        color: '#0D61A9',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: RiSupabaseFill,
+        size: 48,
+        color: 'green',
+      },
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+    ],
   },
 
   {
@@ -12,6 +54,38 @@ export const listaProjetos = [
     titulo: 'App Paycheck',
     descricao: 'Aplicativo Android para geração e consulta de holerites',
     href: 'https://play.google.com/store/apps/details?id=com.js.mobile.paycheck',
+    tech: [
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: SiFirebase,
+        size: 45,
+        color: 'yellow',
+      },
+      {
+        icon: IoLogoNodejs,
+        size: 45,
+        color: 'green',
+      },
+      {
+        icon: SiExpress,
+        size: 45,
+        color: 'green',
+      },
+      {
+        icon: FaGitAlt,
+        size: 45,
+        color: '#F4511E',
+      },
+    ],
   },
 
   {
@@ -20,6 +94,29 @@ export const listaProjetos = [
     descricao:
       'Aplicativo Android para busca de prestadores de serviços, ao qual disponiblizam em determinada área',
     href: 'https://play.google.com/store/apps/details?id=com.cleanservicos',
+    tech: [
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: SiFirebase,
+        size: 45,
+        color: 'yellow',
+      },
+
+      {
+        icon: FaGitAlt,
+        size: 45,
+        color: '#F4511E',
+      },
+    ],
   },
 
   {
@@ -28,5 +125,99 @@ export const listaProjetos = [
     descricao:
       'Sistema Web para gerenciamento e controle de estoque de insumos agricolas',
     href: 'https://gestao-agro.vercel.app/',
+    tech: [
+      {
+        icon: FaHtml5,
+        size: 50,
+        color: '#E65100',
+      },
+      {
+        icon: BiLogoTypescript,
+        size: 50,
+        color: '#0D61A9',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: RiSupabaseFill,
+        size: 48,
+        color: 'green',
+      },
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+    ],
+  },
+  {
+    id: 5,
+    titulo: 'Lista de países',
+    descricao:
+      'Site que exibe uma lista de países, consumindo da API REST COUNTRIES',
+    href: 'https://countries-virid-phi.vercel.app/',
+    tech: [
+      {
+        icon: RiNextjsFill,
+        size: 50,
+        color: 'cyan',
+      },
+      {
+        icon: FaHtml5,
+        size: 50,
+        color: '#E65100',
+      },
+      {
+        icon: BiLogoTypescript,
+        size: 50,
+        color: '#0D61A9',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+    ],
+  },
+  {
+    id: 6,
+    titulo: 'Tattoo Shop',
+    descricao: 'Web site da Tattoo Shop, desafio aplicado da pagina codante.io',
+    href: 'https://tattoo-shop-three.vercel.app/',
+    tech: [
+      {
+        icon: RiNextjsFill,
+        size: 50,
+        color: 'cyan',
+      },
+      {
+        icon: FaHtml5,
+        size: 50,
+        color: '#E65100',
+      },
+      {
+        icon: BiLogoTypescript,
+        size: 50,
+        color: '#0D61A9',
+      },
+      {
+        icon: RiJavascriptFill,
+        size: 50,
+        color: 'yellow',
+      },
+      {
+        icon: FaReact,
+        size: 48,
+        color: 'cyan',
+      },
+    ],
   },
 ];
--- src/componentes/projetos/projetos.jsx
+++ src/componentes/projetos/projetos.jsx
@@ -3,20 +3,34 @@ import './projetos.css';
 
 const Projetos = () => {
   return (
-    <section className="container_projetos">
-      <article className="titulo_projetos">
+    <section className='container_projetos'>
+      <article className='titulo_projetos'>
         <span>{'{ Projetos }'}</span>
       </article>
 
-      <section className="lista_projetos">
+      <section className='lista_projetos'>
         {listaProjetos.map((proj) => {
+          const TechIcon = proj.tech[0].icon;
           return (
-            <aside className="projeto" key={proj.id}>
+            <aside className='projeto' key={proj.id}>
               <div>
                 <h3>{proj.titulo}</h3>
               </div>
+
               <div>
                 <p>{proj.descricao}</p>
+                <div>
+                  <p>Tecnologias</p>
+                  {proj.tech.map((item, index) => {
+                    const Icon = proj.tech[index].icon;
+                    return (
+                      <Icon
+                        size={proj.tech[index].size}
+                        color={proj.tech[index].color}
+                      />
+                    );
+                  })}
+                </div>
                 <p>
                   <a href={proj.href}>Visualizar</a>
                 </p>
